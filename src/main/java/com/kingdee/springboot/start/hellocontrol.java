354ab5339c2b56bc57b050bb05e19514b07428ea
a278779191 <18001164010@163.com>
2019-07-07 15:28:22 +0800

    //修改测试

---
+++ src/main/java/com/kingdee/springboot/start/hellocontrol.java
@@ -0,0 +1,10 @@
+package com.kingdee.springboot;
+
+
+public class hellocontrol {
+
+    public static void main(String[] args) {
+        System.out.println("我爱你");
+    }
+
+}
---
+++ src/test/java/com/kingdee/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,10 @@
+package com.kingdee.springboot;
+
+
+
+public class SpringbootApplicationTests {
+
+    public static void main(String[] args) {
+        System.out.println("aio");
+    }
+}
